f94d3633e1e42401ef6e377bb7af372eaea53a56
Kiko <fjavier.gelabert@gmail.com>
2024-10-23 12:21:41 -0700

    Revert "WIP Starting Client Side processing"
    This reverts commit 58069a4ad74d8179598d6313314a68a26ceb81ae.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { VideoTranscriberComponent } from "@/components/video-transcriber";
+import { TranscribePage } from "@/components/app-page";
 
-export default function PageComponent() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Video Processing</h1>
-      <VideoTranscriberComponent />
-    </div>
-  );
+export default function Page() {
+  return <TranscribePage />;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-
-    config.module.rules.push({
-      test: /\.worker\.ts$/,
-      loader: "worker-loader",
-      options: {
-        filename: "static/[hash].worker.js",
-        publicPath: "/_next/",
-      },
-    });
-
-    return config;
+  output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: [
+      "sharp",
+      "onnxruntime-node",
+      "@ffmpeg-installer/ffmpeg",
+    ],
   },
 };
 
